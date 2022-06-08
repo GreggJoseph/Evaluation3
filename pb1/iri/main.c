e256e8696cc27c1d454ce3a8d5ca934129c8e9be
Gk Fen <gfenton06@gmail.com>
2022-06-08 10:40:36 -0400

    Read thoroughly the 00readme.txt file to get the details to get IRI_SUB() call to work.  Added proper calls to the read_ig_rz() & readapf107() routines.

--- pb1/iri/main.c
+++ pb1/iri/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 
 // iri_sub Function Prototype declaration
@@ -20,7 +21,12 @@
     
 extern void iri_sub_ (int *, int *, float *, float *, int *, int *, float *, float *,
                float *, float *, float [][20], float *);
-    
+extern void read_ig_rz_ ();
+extern void readapf107_ ();
+float wp(float);
+
+#define PI 3.1415926535897932
+
 int main(void) {
 
     int    i, j, nalt=21;
@@ -33,24 +39,29 @@ int main(void) {
     // Rows and columns between C and Fortran are switched, meaning
     // major order for double arrays is swapped between the C & FORTRAN
     float  outf[1000][20], oarr[100];
-    //bool   jf[50] = { true }; //only sets the 1st element to true
-    int   jf[50];
+    int    jf[50];
     
+    //c Please also make sure to use the default setting for the switches jf
+    //c as noted in this comment section further down:
+    //c       jf(4,5,6,21,23,28,29,30,33,35,39,40,47)=.false. all others .true.
     for( i=0 ; i<50 ; i++ ){
-        jf[i] = 1;
-        if(i>2 && i<6)   jf[i] = 0;
-        if(i==7)         jf[i] = 1; // .true. for foF2
-        //if(i>20 && i<23) jf[i] = 0;
-        if(i==25)        jf[i] = 1; // .true. for foF2
-        //if(i>26 && i<30) jf[i] = 0;
-        //if(i>31 && i<35) jf[i] = 0;
+        jf[i] = 1; //sets all to true
+        
+        if(i>2 && i<6)   jf[i] = 0; // jf[4,5,6] = false
+        if(i==20)        jf[i] = 0; // jf[21] = false
+        if(i==22)        jf[i] = 0; // jf[23] = false
+        if(i>26 && i<30) jf[i] = 0; // jf[28,29,30] = false
+        if(i==32)        jf[i] = 0; // jf[33] = false
+        if(i==34)        jf[i] = 0; // jf[35] = false
+        if(i>37 && i<40) jf[i] = 0; // jf[39, 40] = false
+        if(i==46)        jf[i] = 0; // jf[47] = false
     }
     
     
-    jmag = 1;
+    jmag = 0;
     lat = 37.8; lon = 75.4;
-    iyyyy = 2021; mmdd = 321; dhour = 11.0;
-    //iyyyy = 2021; mmdd = 322; dhour = 23.0;
+    iyyyy = 2021; mmdd = 303; dhour = 11.0;
+    //iyyyy = 2021; mmdd = 304; dhour = 23.0;
     hbeg = 80.0; //km
     hend = 600.0; //km
     hstp = 26.0; //km
@@ -69,29 +80,55 @@ int main(void) {
     printf("%d %f %f %d %d %f", jmag, lat, lon, iyyyy, mmdd, dhour);
     printf("\n%f %f %f", hbeg, hend, hstp );
     
-    printf("\nBefore Fortran subroutine...\n");
+    
+    read_ig_rz_();
+    readapf107_();
+    printf("\n\n * Before Fortran subroutine...\n");
+    
     iri_sub_(jf,    &jmag, &lat,  &lon, &iyyyy, &mmdd, &dhour,
              &hbeg, &hend, &hstp, outf, oarr);
-    printf("After Fortran subroutine...\n\n");
-
-    printf("EDP Peak=%10.3f [m^-3] at Height=%5.1f [km]\n",  oarr[5-1], oarr[6-1]);
-    printf("Lat= %10.3f, Lon= %5.1f\n",  oarr[28-1], oarr[32-1] );
-
-    /*
-    printf("Altitude (km)    Ne (m^-3)\n");
     
+    printf("\n *After Fortran subroutine...\n\n");
+
+    
+    
+    printf("EDP Peak=%10.3f [m^-3] at Height=%5.1f [km]\n",  oarr[5-1], oarr[6-1]);
+    printf("Geo Lat= %10.3f, Geo Lon= %5.1f\n",  oarr[28-1], oarr[32-1] );
+
+
+
+    // electron density (frequency MHz), ion temp (K)
+    printf("\n   Alt (km),  Plasma Freq. (MHz),  ION Temp (K)\n");
+
     for( i=0; i<nalt; i++) {
-        for( j=0 ; j<11 ; j++ ) {
+        printf("%10.3f, %15.7f, %15.7f\n", alt[i], wp(outf[i][1-1])/(2.0*PI), outf[i][3-1]);
+    }
+    
+    /*
+    for( i=0; i<nalt; i++) {
+        for( j=0 ; j<20 ; j++ ) {
             printf("%10.3f, %15.7f : \n", alt[i], outf[i][j]);
         }
         printf("\n");
     }
+     */
     
+    /*
     for( i=0; i<100; i++) {
         printf("%10.3f\n", oarr[i]);
     }
      */
+
     
     return 0;
 }
 
+// Plasma angular Frequency function, takes an Electron number density value (M^3)
+float wp(float ne) {
+    float e = 1.602176462e-19;
+    float e0 = 8.854187817e-12;
+    float me = 9.10938188e-31;
+    
+    // return plasma frequency in MHz
+    return( sqrt((ne * e*e) / (e0 * me)) * 1.0e-6);
+}
